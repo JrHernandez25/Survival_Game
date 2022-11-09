5180208c9e58b82b5f7714368975a36ccceee2d9
Jr Hernandez <jrhernande25@g.crookcountyschools.org>
2022-11-09 14:36:07 -0800

    Started adding things to the javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+alert("Before you begin the game you get to pick a name for your character");
+
+var username = prompt("Enter Your character's name")
+
+alert('Alright lets get you into the game ' + username);
+
+alert("You wake up next to a camp fire only to find out you have been tied up and all your gear and money have been stolen. You kind of wonder why the theifs didn't kill you but you quickly snap out of your thoughts and start looking for ways to get your self untied. You see next to the campfire a dull knife that could be used to cut yourself out the ropes, but you also remember that you can whistle for you wolf pet to come get you out of the rope.")
+
+var numb = prompt("So what is it going to be either 1)The knife or 2)Call for your pet wolf")
+
+if(numb = 1){
+    alert("You choose to use the dull knife, it takes a few hours but the knife is eventually able to cut you free so you pick yourself up and head over to an abandon gas station where you know you can find things that you can craft a weapon.")
+}
